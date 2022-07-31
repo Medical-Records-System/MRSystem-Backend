62c049e896679fbe069ee65cdf3c8ed87f7772c1
Jean-Dv <mrjunior127@gmail.com>
2022-07-31 17:49:41 -0500

    Added disconnect database and delete mongo memory server

--- __test__/home.spec.ts
+++ __test__/home.spec.ts
@@ -1,6 +1,7 @@
 import request from 'supertest'
 
 import { appServer, routePrefix } from '../app'
+import { MongoService } from '../services/mongoDb'
 
 describe(`GET ${routePrefix}/ping`, () => {
   test('should return 200 and text pong', async () => {
@@ -9,4 +10,9 @@ describe(`GET ${routePrefix}/ping`, () => {
     expect(response.statusCode).toBe(200)
     expect(response.body.message).toMatch('pong')
   })
+})
+
+afterAll(async () => {
+  const mongoService = MongoService.getInstance
+  await mongoService.disconnnect()
 })
