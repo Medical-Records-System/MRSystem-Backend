53d9c38210663c7c806d792e79707eb2f4cf1bca
Jean-Dv <mrjunior127@gmail.com>
2022-08-02 15:04:05 -0500

    Added logger into app.ts

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import { Server } from './bin/server'
 const server = Server.getInstance
 const appServer = server.app
 const routePrefix = server.routePrefix
+const logger = server.logger
 server.start()
 
-export { server, appServer, routePrefix }
+export { server, appServer, routePrefix, logger }
