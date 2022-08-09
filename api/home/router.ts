94ff2cef6e3cad70a7c8a424e02a4024deaead1f
Jean-Dv <mrjunior127@gmail.com>
2022-08-09 18:09:56 -0500

    MRS-4 Added route new for ping with middleware is admin

--- api/home/router.ts
+++ api/home/router.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
 
 import { HomeHttpHandler } from './http'
+import { MiddlewareJWT } from '../../middlewares/jwtToken'
+
+const middlewareJwt = MiddlewareJWT.getInstance
 
 export const homeRouter = Router()
 const homeHttpHandler = new HomeHttpHandler()
 
 homeRouter.route('/')
   .get(homeHttpHandler.getPing)
+
+homeRouter.route('/admin')
+  .get([middlewareJwt.isAdmin], homeHttpHandler.getPing)
